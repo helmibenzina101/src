73612840d4578e46a53f6bcf64f0752b3a27f2aa
helmibenzina101 <helmi.benzina@gmail.com>
2024-07-29 17:02:58 +0100

    first commit

---
+++ test/java/com/rifund/rifund/RifundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifund.rifund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RifundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
